54673cb653cb57ac3d34bb09dec9970e96bd4c6a
Dejan Fajfar <dejan@fajfar.com>
2023-09-29 20:51:16 +0200

    Added a simple script to run all examples

---
+++ run_all_examples.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##
+## Rudimentary script to run all examples in the project
+## Just a convenience to save me som typing
+##
+
+##
+# Shell color definitions
+RED='\033[0;31m'
+PURPLE='\033[0;34m'
+NC='\033[0m' # No Color
+
+##
+# Print a nice section header for each sample beeing run
+function sample_header {
+    echo -------------------------------------
+    printf "${PURPLE}$1${NC}\n"
+    echo -------------------------------------
+}
+
+sample_header simple
+cargo run --package uberbyte --example simple 
+
+sample_header set
+cargo run --package uberbyte --example set
+
+sample_header "check mask set"
+cargo run --package uberbyte --example check_mask_set
+
+sample_header "shift"
+cargo run --package uberbyte --example shift
