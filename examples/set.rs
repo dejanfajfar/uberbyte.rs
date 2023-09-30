40d63eee77c25a1405e9d4f4d05b52592e143077
Dejan Fajfar <dejan@fajfar.com>
2023-09-30 19:16:21 +0200

    Update set.rs

--- examples/set.rs
+++ examples/set.rs
@@ -1,9 +1,15 @@
-use uberbyte::UberByte;
+use uberbyte::{UberByte, FIFTH_BIT_MASK};
 
 fn main() {
+    // set with with copy
     let my_byte = UberByte::MIN;
-
     let my_set_byte = my_byte.set(0b_0001_0001);
 
     println!("original {:b}, after set {:b}", my_byte, my_set_byte);
+
+    // set mut
+    let mut my_byte2 = UberByte::MIN;
+    my_byte2.set_mut(FIFTH_BIT_MASK);
+
+    println!("Byte after set {:b}", my_byte2);
 }
