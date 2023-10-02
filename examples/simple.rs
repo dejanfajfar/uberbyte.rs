f1b2f9563c9abe0c97d5e143e273c23f3c0b870e
Dejan Fajfar <dejan@fajfar.com>
2023-10-02 17:53:01 +0200

    Added a simple example to show basic functionality

---
+++ examples/simple.rs
@@ -0,0 +1,17 @@
+use uberbyte::UberByte;
+
+/// A simple example showing how to check if a given bit in the byte is set
+
+fn main() {
+    let my_byte: UberByte = UberByte::from(42);
+
+    println!("{:b}", my_byte);
+    for index in 0..7 {
+        if my_byte.is_bit_set(index) {
+            println!("Bit on position {} is set", index);
+        }
+        else {
+            println!("Bit on position {} is not set", index);
+        }
+    }
+}
--- run_all_examples.sh
+++ run_all_examples.sh
@@ -19,14 +19,17 @@ function sample_header {
     echo -------------------------------------
 }
 
+sample_header "Simple sample"
+cargo run --package uberbyte --example simple
+
 sample_header "Create a UberByte"
 cargo run --package uberbyte --example create
 
 sample_header simple
 cargo run --package uberbyte --example is_set 
 
-sample_header set
-cargo run --package uberbyte --example set
+sample_header "Set clear flip"
+cargo run --package uberbyte --example set_clear_flip
 
 sample_header "check mask set"
 cargo run --package uberbyte --example check_mask_set
